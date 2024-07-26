6852f5be67aa25c03883a835795aa86cfa3c67c5
HIBIKI_5201 <takuto5201@gmail.com>
2024-07-26 20:23:12 +0900

    [update] キャラとセリフのひもづけを可能に

--- Assets/Script/StorySystem/StorySystem.cs
+++ Assets/Script/StorySystem/StorySystem.cs
@@ -7,20 +7,48 @@ public class StorySystem : MonoBehaviour
     [SerializeField]
     Text textBox;
 
+    [Header("オブジェクト")]
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    GameObject grim;
+
+    [SerializeField]
+    GameObject palpal;
+
+    [SerializeField]
+    GameObject otherOne;
+    [SerializeField]
+    string otherOneName;
+    static string OtherOneName;
+
+
+    [SerializeField]
+    GameObject otherTwo;
+    [SerializeField]
+    string otherTwoName;
+    static string OtherTwoName;
+
     enum Character
     {
         player,
         grim,
         palpal,
-        other
+        otherOne,
+        otherTwo,
     }
 
     [SerializeField]
-    List<Character> activeChara;
+    List<Character> SayChara;
 
-    Dictionary<Character, string> CharacterNames = new() 
-    { {Character.player, MainSystem._playerName },
-        {Character.grim, "グリム" }
+    Dictionary<Character, string> CharacterNames = new()
+    { 
+        {Character.player, MainSystem._playerName },
+        {Character.grim, "グリム" },
+        {Character.palpal, "パルパル" },
+        {Character.otherOne, OtherOneName },
+        {Character.otherTwo, OtherTwoName },
     };
 
     [SerializeField]
@@ -41,7 +69,7 @@ public class StorySystem : MonoBehaviour
         }
         else
         {
-            Debug.Log(text[textNumber]);
+            Debug.Log(CharacterNames[SayChara[textNumber]] + text[textNumber]);
             //textBox.text = text[textNumber];
         }
 
