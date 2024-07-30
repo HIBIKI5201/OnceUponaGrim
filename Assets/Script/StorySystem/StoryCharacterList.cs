2788c2542e029eb9dbb5f8b7e5a9cb3858a1af23
HIBIKI_5201 <takuto5201@gmail.com>
2024-07-30 14:24:46 +0900

    [update] StorySystemを大幅に改良

---
+++ Assets/Script/StorySystem/StoryCharacterList.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class StoryCharacterList
+{
+    public string characterType;
+    public GameObject gameObject;
+    public string characterName;
+}
