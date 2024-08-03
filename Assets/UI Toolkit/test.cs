76952fb0a5f48784a5a37612e6322061aaf7a767
HIBIKI_5201 <takuto5201@gmail.com>
2024-08-03 19:16:31 +0900

    [add] UI ToolKitをテスト ボタンをアニメーションを設計

---
+++ Assets/UI Toolkit/test.cs
@@ -0,0 +1,59 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Test : VisualElement
+{
+    private Label _characterName;
+    private Label _description;
+    private VisualElement _thumbnail;
+
+    StorySystem _storySystem;
+
+    private string CharacterName => _characterName?.text;
+    private string Description => _description?.text;
+
+    public new class UxmlFactory : UxmlFactory<Test, UxmlTraits> { }
+
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+        public UxmlStringAttributeDescription characterName = new UxmlStringAttributeDescription { name = "character-name", defaultValue = "Name" };
+        public UxmlStringAttributeDescription description = new UxmlStringAttributeDescription { name = "description", defaultValue = "Description" };
+        public UxmlAssetAttributeDescription<Texture2D> thumbnail = new UxmlAssetAttributeDescription<Texture2D> { name = "thumbnail" };
+        public UxmlAssetAttributeDescription<GameObject> storySystem = new UxmlAssetAttributeDescription<GameObject> { name = "story-system" };
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+            var card = ve as Test;
+
+            card._characterName.text = characterName.GetValueFromBag(bag, cc);
+            card._description.text = description.GetValueFromBag(bag, cc);
+            card._thumbnail.style.backgroundImage = thumbnail.GetValueFromBag(bag, cc);
+        }
+    }
+
+    public Test()
+    {
+        var treeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI Toolkit/Test.uxml");
+        var container = treeAsset.Instantiate();
+        hierarchy.Add(container);
+
+        _characterName = container.Q<Label>("Name");
+        _description = container.Q<Label>("Description");
+        _thumbnail = container.Q<VisualElement>("Thumbnail");
+
+        var card = container.Q<VisualElement>("Root");
+        card.AddManipulator(new Clickable(() => OnClick()));
+    }
+
+    public void OnClick()
+    {
+        if (_storySystem == null)
+        {
+            _storySystem = GameObject.FindAnyObjectByType<StorySystem>();
+        }
+        
+        _storySystem.NextText();
+    }
+}
